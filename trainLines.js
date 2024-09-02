0055ffca6030e312a8b38cbf0dedfae787dacf4f
RolusC5B <92598673+RolusC5B@users.noreply.github.com>
2024-09-02 15:28:17 -0400

    Added file
    the git push did not upload the java script file so I am doing it now.

---
+++ trainLines.js
@@ -0,0 +1,90 @@
+//setting up async/await ask functionality for the command line
+const readline = require("readline");
+const readlineInterface = readline.createInterface(
+    process.stdin,
+    process.stdout
+);
+
+function ask(questionText) {
+    return new Promise((resolve, reject) => {
+        readlineInterface.question(questionText, resolve);
+    });
+}
+
+//YOUR CODE GOES HERE
+class Line {
+    constructor(color) {
+        this.color = color;//it prints this line as text
+        this.connections = [];//it prints this line as text
+    }
+
+    addConnections(lines){
+        this.connections.push(...lines);
+    }
+}
+
+class Person {
+    constructor(startingLine) {
+        this.currentLine = startingLine;
+        this.name = null;
+    }
+
+    transferLines(line){
+        this.currentLine = line;
+    }
+}
+
+let blue = new Line("blue");
+let green = new Line("green");
+let yellow = new Line("yellow");
+let red = new Line("red");
+let purple = new Line("purple");
+
+blue.addConnections([green, red]);
+green.addConnections([blue, yellow]);
+yellow.addConnections([green, red, purple]);
+red.addConnections([blue, yellow]);
+purple.addConnections([yellow]);
+
+let user = new Person(blue);
+
+async function main() {
+    //YOUR RIDE GOES HERE
+    //introduce user to our program
+    console.log("Hello! Welcome to our subway!");
+    //ask user what their name is
+    user.name = await ask("What is your name? ");
+    //TODO: what happens if the user's name is ""?
+    //tell the user where they are starting
+    console.log(`Hello ${user.name}! You are on the ${user.currentLine.color}`)
+
+    //let the user travel from line to line
+    //ask the user for a destination to travel to
+    
+    let destLine = undefined;
+    do {
+        const destination = await ask("Where would you like to go? ");
+    //find the line object that the user provided in our current line's connections array
+    destLine = user.currentLine.connections.find((line) => line.color === destination);
+    //TODO: what happens if destination is "Red"?
+    //TODO: how does our user know where they can travel?
+    //console.log(destLine)
+    
+    if (destLine === undefined) {
+            console.log(`You can't go from ${user.currentLine.color} to ${destination}.`);
+    
+        }
+    } while (destLine === undefined) //test if the user can travel to their destination
+        
+    user.transferLines(destLine);
+    console.log(`You are now on the ${user.currentLine.color} line.`);
+    //TODO let the user move lines more than one time
+    
+
+    //end program
+    process.exit();
+}
+
+main();
+
+//process.exit();//if this is outside of the function then the program wont run fully.
